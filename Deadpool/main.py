953af35137647ff51b7e507d43f9c2ce57f1c192
Aluno_Programador1 <aluno@gmail.com>
2025-08-04 17:29:02 -0300

    Aula de Hoje

--- Deadpool/main.py
+++ Deadpool/main.py
@@ -1,6 +1,20 @@
 def main():
-   idade = input("Digite a sua idade")
+      idade = int (input("me diga a sua idade: "))
+
+      if idade >=  18:
+         print("você pode entrar")
+
+      elif idade >= 16:
+         acomp = input("você esta acompanhado?")
+
+         if acomp == "sim":
+               print("você pode entrar")
+         else :
+               print ("você não pode entar")
+         
+      else :
+            print("entrada proibida")
 
 
-    return 0
-main()
+      return 0
+main() 
